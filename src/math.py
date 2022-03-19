f629a699d62a9c81825ff4d65e792f3c137709b6
M12oooo <88971780+M12oooo@users.noreply.github.com>
2022-03-19 20:51:27 +0530

    handle divide by zero

--- src/math.py
+++ src/math.py
@@ -3,11 +3,13 @@ def add(x,y):
 def multiply(x,y):
     return x*y    
 def divide(x,y):
+    if y==0:
+        return 0
     return x/y
 def subtract(x,y):
     return x-y;        
-if __name__== '_main_':
+if __name__== '__main__':
     print(add(1,2))
     print(multiply(1,2))
-    print(divide(1,2))
+    print(divide(1,0))
     print(subtract(1,2))   
